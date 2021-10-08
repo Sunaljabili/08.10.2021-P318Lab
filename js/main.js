86727c1688da30d62ed6613783e863ab6a7a962d
Sunaljabili <you@example.com>
2021-10-08 21:25:53 +0400

    Tasks Completed

---
+++ js/main.js
@@ -0,0 +1,57 @@
+// Task-1
+
+// function sum(arr){
+//     let result = 0;
+//     for(let i = 0; i < arr.length ; i++){
+//         if(typeof(arr[i])!=="string"){
+//         result+=arr[i];
+//         }
+//     }
+//     return result;
+// }
+// console.log(sum([2,5,7,"Rail",'a',8,4,5,false]));
+
+// Task-2
+
+// function equalTo (a,b)
+// {
+//     if (a+b===100 || a===100 || b===100) {
+//         return true;
+
+//     }
+//     return false;
+// }
+// console.log(equalTo(100,90));
+
+// Task-3
+
+// function getCredit(credit,month){
+//     let part =credit%month;
+//     let payment=(credit-part)/month;
+//     for (var i = 1; i < month; i++) {
+//         console.log(`${i}-ci ay odenish ${payment} azn`)
+//     }
+//     console.log(`${i}-ci ay odenish ${payment +part} azn`)
+// }
+
+// getCredit(200,3)
+
+// Task-4
+
+// function sameCase(str) {
+//   if (str === str.toLowerCase() || str === str.toUpperCase()) {
+//     return true;
+// }
+//   return false;
+// }
+// console.log(sameCase("FEQAN"))
+
+// let number =7;
+// console.log(typeof number);
+// let isMarried=true;
+// console.log(typeof isMarried);
+
+// let firstname="Vuqar";
+// console.log(typeof firstname);
+
+// console.log(typeof typeof null);
